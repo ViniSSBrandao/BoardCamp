4cc6fc500a8935a7ebd31a2c29ac4c6e240c89af
vini <vinicius.brandaot@gmail.com>
2023-02-13 00:54:42 -0300

    fixed delete route for inexistant rentals

--- src/middlewares/rentalsMiddlewares/rentalDeleteMiddleware.js
+++ src/middlewares/rentalsMiddlewares/rentalDeleteMiddleware.js
@@ -7,14 +7,15 @@ const { id } = req.params
 console.log("a")
 try{
     const rental = await db.query(`SELECT * FROM rentals WHERE id = $1;`, [id])
+    console.log(rental.rows)
 
+    if(rental.rowCount == 0){
+        return res.sendStatus(404)
+    }
     if(!rental.rows[0].returnDate){
         return res.sendStatus(400)
     }
     
-    if(!rental.rowCount){
-        return res.sendStatus(404)
-    }
     next();
 }catch(error){
     return res.status(500).send(error.message)
