8ede84db73ac72c2f356f1044914e27ba23c0ada
vini <vinicius.brandaot@gmail.com>
2023-02-13 01:27:47 -0300

    fixed value

--- src/controllers/rentals/postReturnController.js
+++ src/controllers/rentals/postReturnController.js
@@ -6,18 +6,19 @@ import db from "../../config/database/databaseConnection.js"
 export async function returnPost(req, res){
     const {id} = req.params
     try{
-        const rentFind = await db.query(`SELECT * FROM rentals WHERE id = ${id}`) 
-        const { rentDate, daysRented, originalPrice} = rentFind.rows[0]
-        const pricePerDay = daysRented/originalPrice;
-        console.log(rentFind.rows[0])
+        const rentFind = await db.query(`SELECT * FROM rentals WHERE id = ${id}`)
+        const { rentDate, daysRented, gameId } = rentFind.rows[0]
+        const game = await db.query(`SELECT * FROM games WHERE id = $1;`, [gameId])
+        const {pricePerDay} = game.rows[0]
+        
         
         const returnDate = dayjs(new Date()).format("YYYY-MM-DD");
         const delayedDaysMs = new Date(returnDate) - new Date(rentDate)
-        const delayedDays = Math.floor(delayedDaysMs/1000/60/60/24)
+        const delayedDays = Math.floor(delayedDaysMs/86400000)
 
-        let delayFee = (delayedDays - daysRented) * 2 * pricePerDay;
+        let delayFee = (delayedDays - daysRented) * (2 * pricePerDay);
         if(delayFee<0){ delayFee = 0 }
-        console.log(pricePerDay)
+        console.log(delayedDays)
         console.log(delayFee)
         const returnRentals = await db.query(`UPDATE rentals SET "returnDate"=$1,"delayFee"=$2 WHERE id=$3;`, [
             returnDate,
