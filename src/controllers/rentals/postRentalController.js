7f081ef33d32f2d57455633beec5a38693c73537
vini <vinicius.brandaot@gmail.com>
2023-02-12 21:01:59 -0300

    find game in stack post route

--- src/controllers/rentals/postRentalController.js
+++ src/controllers/rentals/postRentalController.js
@@ -1,10 +1,11 @@
 import db from "../../config/database/databaseConnection.js"
 export async function rentalPost(req, res){
-    // const { name, image, stockTotal, pricePerDay } = req.body
-    // console.log(name)
+    const  {customerId,gameId,daysRented} = req.body
+ 
     try{
-        // const insertGame = await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1,$2,$3,$4);`, [name, image, stockTotal, pricePerDay]) 
-        return res.sendStatus(201)
+        const game = await db.query(`SELECT * FROM games WHERE id = ${gameId}`) 
+        // const insertGame = await db.query(`INSERT INTO rentals (name, image, "stockTotal", "pricePerDay") VALUES ($1,$2,$3,$4);`, [customerId, gameId, daysRented]) 
+        return res.status(201).send(game.rows)
     }catch(error){
         return res.status(500).send(error.message)
     } 
