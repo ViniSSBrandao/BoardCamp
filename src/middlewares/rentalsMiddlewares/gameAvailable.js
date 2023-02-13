848c86a5faa64ab53af89eb91620d52ac62f500b
vini <vinicius.brandaot@gmail.com>
2023-02-12 21:43:13 -0300

    verify if all games were rented

--- src/middlewares/rentalsMiddlewares/gameAvailable.js
+++ src/middlewares/rentalsMiddlewares/gameAvailable.js
@@ -7,9 +7,10 @@ console.log(req.body)
 
 try{
     const game = await db.query(`SELECT * FROM games WHERE id = $1;`, [gameId])
+    const rentedAmount = await db.query(`SELECT * FROM rentals WHERE "gameId" = ${gameId}`)
     // let gamesAvailable = game[0].
-    console.log(game.rowCount)
-    if(!game.rowCount){
+    console.log(rentedAmount.rowCount)
+    if(!game.rowCount || rentedAmount.rowCount>=game.rows[0].stockTotal){
         return res.sendStatus(400)
     }
     
